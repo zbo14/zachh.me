32c21262ff01c97a76b233bd4bd07b475508865c
zach balder <zmbalder@gmail.com>
2021-06-09 12:50:24 -0400

    add new section to projects page

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -10,6 +10,9 @@
     <h2 class="italic mb-4">I like tackling complex problems</h2>
     <p>Reading through specifications, reviewing other implementations, and building my own. These steps help cement my understanding of how a system works and why it was designed the way it was. Not too long ago, I created <g-link href="https://github.com/zbo14/triple-double">my own implementation</g-link> of the Double Ratchet and Extended Triple Diffie-Hellman Algorithms, allowing users to send encrypted messages to each other over WebSockets.</p>
     <br>
+    <h2 class="italic mb-4">I'm interested in assisting other developers</h2>
+    <p>In <b>2021</b>, I've worked on a <g-link href="https://github.com/zbo14/tls-refresh">couple</g-link> <g-link href="https://github.com/zbo14/certnode"> projects</g-link> to make TLS certificate 🔐 issuance and renewal more accessible. This effort piggybacks on a much larger undertaking by the <g-link href="https://www.abetterinternet.org/">ISRG</g-link> and other organizations to increase Internet security. Developers have many tools at their disposal to secure their websites and web services and lately, I've been focused on ways to make this tooling even easier to use.</p>
+    <br>
     <h2 class="italic mb-4">Want to work on something together?</h2>
     <p>I'm always excited for opportunities to collaborate on projects!</p>
     <p>Feel free to reach out on social or email: zmbalder{at}gmail{dot}com.</p>
